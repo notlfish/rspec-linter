b71810f8d334a861da6ff7b2f745992bc6098ff7
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-05-20 13:48:18 -0300

    Fix bin/rspec-linter
    Changed symlink into a ruby file, calling lib/rspec_linter.rb and running
    from there.

--- lib/rspec_linter.rb
+++ lib/rspec_linter.rb
@@ -1,39 +1,42 @@
-#!/usr/bin/env ruby
-require_relative '../lib/linter'
+require_relative 'linter'
 
-def test_file?(filename)
-  %w[_test.rb _spec.rb].any? { |suffix| filename.end_with? suffix }
-end
+class RspecLinter
+  private
 
-def arguments_usage
-  <<~ARGS
-    rspec-linter
-    usage:
-      rspec-linter: looks for test files in . and ./spec and runs the linter on them
-      rspec-linter filename: runs the linter on ./filename'
-  ARGS
-end
-
-def test_files(working_directory)
-  tests = []
-  files = Dir.children(working_directory)
-  files.each { |filename| tests.push(filename) if test_file?(filename) }
-  if files.include? 'spec'
-    Dir.chdir('spec')
-    tests += test_files(Dir.pwd)
+  def test_file?(filename)
+    %w[_test.rb _spec.rb].any? { |suffix| filename.end_with? suffix }
   end
-  tests
-end
 
-def run
-  case ARGV.length
-  when 0
-    puts Linter.new(test_files(Dir.pwd)).run
-  when 1
-    puts Linter.new([ARGV[0]]).run
-  else
-    puts arguments_usage
+  def arguments_usage
+    <<~ARGS
+      rspec-linter
+      usage:
+        rspec-linter: looks for test files in . and ./spec and runs the linter on them
+        rspec-linter filename: runs the linter on ./filename'
+    ARGS
+  end
+
+  def test_files(working_directory)
+    tests = []
+    files = Dir.children(working_directory)
+    files.each { |filename| tests.push(filename) if test_file?(filename) }
+    if files.include? 'spec'
+      Dir.chdir('spec')
+      tests += test_files(Dir.pwd)
+    end
+    tests
+  end
+
+  public
+
+  def run(args)
+    case args.length
+    when 0
+      puts Linter.new(test_files(Dir.pwd)).run
+    when 1
+      puts Linter.new([args[0]]).run
+    else
+      puts arguments_usage
+    end
   end
 end
-
-run if __FILE__ == $PROGRAM_NAME
